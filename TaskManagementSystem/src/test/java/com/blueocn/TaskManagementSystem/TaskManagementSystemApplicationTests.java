6fcb99c10cdc808f9a85ac00d956503e258b6420
BlueOCN <robgonzalezmu@gmail.com>
2025-05-27 19:06:21 -0600

    Update - Spring Boot Project Scaffold added.

---
+++ TaskManagementSystem/src/test/java/com/blueocn/TaskManagementSystem/TaskManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blueocn.TaskManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
